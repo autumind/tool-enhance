b6289f1bf478c5976e404771aff68137a7953957
shen.zhibing <shen.zhibing@mobcb.com>
2017-09-14 19:41:53 +0800

    Initial commit.

---
+++ src/main/java/com/iylyi/tools/App.java
@@ -0,0 +1,13 @@
+package com.iylyi.tools;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
